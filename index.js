ac420f13267a3ab36b6e44ab87098e035ba98ce3
Mownitaa <mabiamownita@gmail.com>
2021-11-13 13:39:19 +0600

    users added

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// // firebase service account
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+// });
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -15,6 +22,23 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 // console.log(uri)
 
+// // verify token
+// async function verifyToken(req, res, next) {
+//     if (req.headers?.authorization?.startsWith('Bearer ')) {
+//         const token = req.headers.authorization.split(' ')[1];
+
+//         try {
+//             const decodedUser = await admin.auth().verifyIdToken(token);
+//             req.decodedEmail = decodedUser.email;
+//         }
+//         catch {
+
+//         }
+
+//     }
+//     next();
+// }
+
 // async function
 async function run() {
     try {
@@ -22,7 +46,9 @@ async function run() {
         const database = client.db('Nailphilia');
         const productsCollection = database.collection('products');
         const reviewsCollection = database.collection('reviews');
-        const purchasesCollection = database.collection('purchases');
+        const usersCollection = database.collection('users');
+
+        // const purchasesCollection = database.collection('purchases');
 
         // GET PRODUCTS API
         app.get('/products', async (req, res) => {
@@ -54,7 +80,7 @@ async function run() {
             console.log('getting specific review', id);
             const query = { _id: ObjectId(id) };
             const review = await reviewsCollection.findOne(query);
-            res.json(service);
+            res.json(review);
         })
 
         // POST REVIEWS API
@@ -67,13 +93,6 @@ async function run() {
             res.json(result);
         });
 
-
-
-        // POST PURCHASES API
-        app.post('/product/:productId', async (req, res) => {
-
-        })
-
         // DELETE API
         //  app.delete('/reviews/:id', async (req, res) => {
         //     const id = req.params.id;
@@ -81,6 +100,55 @@ async function run() {
         //     const result = await reviewsCollection.deleteOne(query);
         //     res.json(result);
         // })
+
+
+
+
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'you do not have access to make admin' })
+            }
+
+        });
     }
     finally {
         // await client.close();
